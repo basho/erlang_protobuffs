2852c5d3ffb86aa11291ed5ebdc9a79f4154e0b0
Nick Gerakines <nick@gerakines.net>
2009-03-04 17:52:51 -0800

    Adding some misc docs

--- eqct/eqct_protobuffs_basic.erl
+++ eqct/eqct_protobuffs_basic.erl
@@ -1,7 +1,12 @@
+%% @doc A basic QuickCheck testing module.
 -module(eqct_protobuffs_basic).
 -compile(export_all). 
 -include_lib("eqc/include/eqc.hrl").
 
+%% @spec int32() -> ok
+%% @doc Uses QuickCheck to test the int32 encoding and decoding component of
+%% protobuffs. It should be noted that this module only tests positives
+%% numbers.
 int32() -> 
     ?FORALL(
         Xs,
@@ -9,6 +14,9 @@ int32() ->
         {{1, Xs}, <<>>} == protobuffs:decode(list_to_binary(protobuffs:encode(1, Xs, int32)), int32)
     ).
 
+%% @spec string() -> ok
+%% @doc Uses QuickCheck to test the string encoding and decoding component
+%% of protobuffs.
 string() ->
     ?FORALL(
         Xs,
